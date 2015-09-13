f1c24545669e86923a9ea1048e892255536bd746
Kahlil Lechelt <hello@kahlil.info>
2015-09-13 20:49:16 +0200

    set default value for page

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const getReviews = require('./lib/getReviews');
 const format = require('./lib/format');
 const chalk = require('chalk');
 
-function fido(config, page) {
+function fido(config, page = 1) {
   config.forEach((cfg) => {
     getReviews(cfg.countries, page, cfg.podcastId, (error, data) => {
       console.log(chalk.bold.black('** ' + cfg.name + ' **'));
