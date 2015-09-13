606a6723cdb334c2dcb340d6de5331c609bb2aa0
Kahlil Lechelt <hello@kahlil.info>
2015-09-13 14:00:00 +0200

    fix test name

--- test/isArray.js
+++ test/isArray.js
@@ -3,7 +3,7 @@ require('babel/register');
 const tap = require('tap');
 const isArray = require('../lib/util/isArray');
 
-tap.test('format', function cb(t) {
+tap.test('isArray', function cb(t) {
   t.plan(1);
   t.equal(isArray({}), false);
   // call t.end() when you're done
