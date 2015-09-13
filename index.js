10c7bd3ea7fb46e65eedcbfcb9c0eeeb123dd165
Kahlil Lechelt <hello@kahlil.info>
2015-09-13 14:04:28 +0200

    remove copyright log and fix template

--- index.js
+++ index.js
@@ -4,10 +4,7 @@ const getReviews = require('./lib/getReviews');
 const format = require('./lib/format');
 const chalk = require('chalk');
 
-
 function fido(config, page) {
-  console.log(chalk.green('\niTunes-Podcast-Reviews\n2015 (c) Reactivists (https://github.com/orgs/reactivepod/teams/reactivists)\nMIT\n'));
-
   config.forEach((cfg) => {
     getReviews(cfg.countries, page, cfg.podcastId, (error, data) => {
       console.log(chalk.bold.black('** ' + cfg.name + ' **'));
