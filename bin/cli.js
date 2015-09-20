4053dde1cb8a42c8c2837686fa291d64c29f43c3
Henning Glatter-Götz <henning@glatter-gotz.com>
2015-09-20 11:30:02 +0200

    refactor(): Fix linting warnings

--- bin/cli.js
+++ bin/cli.js
@@ -1,41 +1,41 @@
 #!/usr/bin/env node
 require('babel/register');
 
-const Yam = require("yam");
+const Yam = require('yam');
 const pathHelper = require('../lib/util/pathHelper');
 const getCurrentPath = pathHelper.getCurrentPath;
 const getUserHomeDirectory = pathHelper.getUserHomeDirectory;
 
-var config = new Yam('fido', {
+const config = new Yam('fido', {
   primary: getUserHomeDirectory(),
-  secondary: getCurrentPath()
+  secondary: getCurrentPath(),
 });
 
 const fido = require('../');
 
-var argv = require('yargs')
+const argv = require('yargs')
   .usage('Usage: fido')
   .option('i', {
-        alias: 'id',
-        demand: false,
-        describe: 'The podcast id to query (single numeric value)'
+    alias: 'id',
+    demand: false,
+    describe: 'The podcast id to query (single numeric value)',
   })
   .option('n', {
-        alias: 'name',
-        demand: false,
-        describe: 'The name of the podcast to query',
-        type: 'string'
+    alias: 'name',
+    demand: false,
+    describe: 'The name of the podcast to query',
+    type: 'string',
   })
   .option('c', {
-        alias: 'countries',
-        demand: false,
-        describe: 'The countries to query (space separated)',
-        type: 'array'
+    alias: 'countries',
+    demand: false,
+    describe: 'The countries to query (space separated)',
+    type: 'array',
   })
   .option('p', {
-        alias: 'pages',
-        demand: false,
-        describe: 'The number of pages to fetch (page size is 50)'
+    alias: 'pages',
+    demand: false,
+    describe: 'The number of pages to fetch (page size is 50)',
   })
   .help('h')
   .alias('h', 'help')
@@ -47,7 +47,7 @@ if (typeof argv.id !== 'undefined') {
   cfg.push({
     id: argv.id,
     name: argv.name,
-    countries: argv.countries
+    countries: argv.countries,
   });
 } else {
   cfg = config.get('podcasts');
