83db101e8368d5acb6f030104190bc63af8d5e77
Kahlil Lechelt <hello@kahlil.info>
2015-10-31 22:23:13 +0100

    show fetching notice when fido starts fetching reviews

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const chalk = require('chalk');
 
 function fido(config, page = 1, fromDate = null) {
   config.forEach((cfg) => {
+    console.log(`fetching reviews for ${cfg.name}...\n`);
     getReviews(cfg.countries, page, cfg.podcastId, (error, data) => {
       console.log(chalk.bold.black('** ' + cfg.name + ' **'));
       console.log(format(data, fromDate));
