f86a7a0d72b009ecd00708391d669c98e0e71ae9
Alexander Plavinski <hello@cilice.me>
2015-11-03 02:09:04 +0100

    Actually breaking this test deserves :clap:

--- test/isArray.js
+++ test/isArray.js
@@ -1,9 +1,8 @@
 const tap = require('tap');
-const isArray = require('lodash.isarray');
 
 tap.test('isArray', function cb(t) {
   t.plan(1);
-  t.equal(isArray({}), false);
+  t.equal(Array.isArray({}), false);
   // call t.end() when you're done
   t.end();
 });
