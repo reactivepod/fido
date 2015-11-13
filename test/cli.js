ad01663c4d8468145e9ae422552e26210187bada
Alexander Plavinski <hello@cilice.me>
2015-11-13 02:01:41 +0100

    Calling cli with node in tests

--- test/cli.js
+++ test/cli.js
@@ -6,7 +6,7 @@ import { readFileSync } from 'fs';
 process.chdir(__dirname);
 
 test('cli version', t => {
-  const cp = spawn('../lib/bin/cli.js', ['--version'], {
+  const cp = spawn('node', ['../lib/bin/cli.js', '--version'], {
     stdio: [process.stdin, null, null],
   });
 
@@ -20,7 +20,7 @@ test('cli version', t => {
 
 test('cli help', t => {
   const fixture = readFileSync('./fixture/help.txt', { encoding: 'utf8' });
-  const cp = spawn('../lib/bin/cli.js', ['--help'], {
+  const cp = spawn('node', ['../lib/bin/cli.js', '--help'], {
     stdio: [process.stdin, null, null],
   });
 
@@ -32,7 +32,7 @@ test('cli help', t => {
 });
 
 test('cli input', t => {
-  const cp = spawn('../lib/bin/cli.js', ['-i 1020286000', '-n Reactive', '-c us de'], {
+  const cp = spawn('node', ['../lib/bin/cli.js', '-i 1020286000', '-n Reactive', '-c us de'], {
     stdio: [process.stdin, null, null],
   });
 
