095fa3ec5228a5dc4c0d0b99d8620dfd120a4ddc
Alexander Plavinski <hello@cilice.me>
2016-01-14 22:15:20 +0100

    feat(lib): introduce getMeta()
    Introduces getMeta(), and deleted meta info from the fido()
    
    BREAKING CHANGE: Meta info no longer included in the reviews

--- src/index.js
+++ src/index.js
@@ -3,20 +3,29 @@ import getMetadata from './util/getMetadata';
 import promiseProps from 'promise-props';
 import { transform } from './util/format';
 
+export function getMeta(config) {
+  const allPromises = config.reduce((all, cfg) => {
+    all[cfg.podcastId] = getMetadata(cfg.podcastId).then(metaInfo => {
+      return {
+        id: cfg.podcastId,
+        meta: metaInfo,
+      };
+    });
+
+    return all;
+  }, {});
+
+  return promiseProps(allPromises);
+}
+
 export default function fido(config, page = 1, fromDate = null) {
   const allPromises = config.reduce((all, cfg) => {
-    all[cfg.podcastId] = Promise.all([
-      getMetadata(cfg.podcastId),
-      getReviews(cfg.countries, page, cfg.podcastId),
-    ]).then(result => {
-      const [meta, review] = result;
-
+    all[cfg.podcastId] = getReviews(cfg.countries, page, cfg.podcastId).then(review => {
       return {
         name: cfg.name,
         fromDate,
         id: cfg.podcastId,
         data: review,
-        meta,
         reviews: transform(review, fromDate),
       };
     });
